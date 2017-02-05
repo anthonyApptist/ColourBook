ad4350317ee217a1a7c2dcc2a8af47de5647b060
anthonyApptist <anthony@theapptist.com>
2017-02-05 11:56:30 -0500

    google and report

---
+++ ColourBook/DataService-Report.swift
@@ -0,0 +1,9 @@
+//
+//  DataService-Report.swift
+//  ColourBook
+//
+//  Created by Anthony Ma on 4/2/2017.
+//  Copyright © 2017 Apptist. All rights reserved.
+//
+
+import Foundation
---
+++ ColourBook/GoogleMap.swift
@@ -0,0 +1,9 @@
+//
+//  GoogleMap.swift
+//  ColourBook
+//
+//  Created by Anthony Ma on 3/2/2017.
+//  Copyright © 2017 Apptist. All rights reserved.
+//
+
+import Foundation
