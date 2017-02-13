4b640704162e25e345a822e89d29b2f3b0f5fe5f
markapptist <mark@theapptist.com>
2017-02-13 17:11:28 -0500

    fixed typo

--- ColourBook/SelectAddressVC.swift
+++ ColourBook/SelectAddressVC.swift
@@ -158,7 +158,7 @@ class SelectAddressVC: CustomVC, UITableViewDelegate, UITableViewDataSource {
             let selectCategory = SelectCategoryVC()
             selectCategory.screenState = self.screenState
             selectCategory.locationName = self.selectedLocation
-            self.present(selectedCategory, animated: true, completion: { 
+            self.present(selectCategory, animated: true, completion: {
                 
             })
         }
